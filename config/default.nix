cd7563062f5826814ee25d320715ca896cf5a113
Lucy <lucy@nix2twink.gay>
2024-10-20 21:24:42 +0200

    added lsp servers

--- config/default.nix
+++ config/default.nix
@@ -5,24 +5,43 @@
   enableMan = true;
 
   opts = {
-     number = true;
-     shiftwidth = 2;
+    number = true;
+    shiftwidth = 2;
   };
 
   viAlias = true;
   vimAlias = true;
 
   colorschemes.tokyonight.enable = true;
- 
-  globals.mapLeader = " ";
 
+  globals.mapLeader = " ";
   plugins = {
     lualine.enable = true;
     telescope.enable = true;
     oil.enable = true;
     treesitter.enable = true;
     luasnip.enable = true;
-    
+    clangd-extensions.enable = true;
+
+    auto-save = {
+      enable = true;
+      settings = {
+        condition = ''
+          function(buf)
+            local fn = vim.fn
+            local utils = require("auto-save.utils.data")
+
+            if utils.not_in(fn.getbufvar(buf, "&filetype"), {'oil'}) then
+              return true
+            end
+            return false
+          end
+        '';
+        debounce_delay = 1000;
+        write_all_buffers = true;
+      };
+    };
+
     lsp = {
       enable = true;
       servers = {
@@ -43,6 +62,11 @@
           enable = true;
           autostart = true;
         };
+        gopls = {
+          enable = true;
+          autostart = true;
+        };
+	ansiblels.enable = true;
       };
     };
 
@@ -59,25 +83,25 @@
 
         mapping = {
           "<CR>" = "cmp.mapping.confirm({ select = true })";
-	  "<Tab>" = ''
-	   cmp.mapping(
-	     function(fallback)
-		if cmp.visible() then
-		 cmp.select_next_item()
-		elseif luasnip.expandable() then
-		 luasnip.expand()
-		elseif luasnip.expand_or_jumpable() then
-		 luasnip.expand_or_jump()
-		elseif check_backspace() then
-		 fallback()
-		else
-		 fallback()
-		end
-	     end
-	   ,
-	   { "i", "s" }
-	 )
-	 '';
+          "<Tab>" = ''
+               cmp.mapping(
+                 function(fallback)
+            	if cmp.visible() then
+            	 cmp.select_next_item()
+            	elseif luasnip.expandable() then
+            	 luasnip.expand()
+            	elseif luasnip.expand_or_jumpable() then
+            	 luasnip.expand_or_jump()
+            	elseif check_backspace() then
+            	 fallback()
+            	else
+            	 fallback()
+            	end
+                 end
+               ,
+               { "i", "s" }
+             )
+          '';
         };
       };
 
